da770310568580142bce9229d9a709953c53e1ef
MJamal12 <malikjamal812@yahoo.com>
2025-04-29 19:11:41 -0400

    fixed launcher

--- scenes/vr_launcher.gd
+++ scenes/vr_launcher.gd
@@ -1,54 +1,20 @@
-[gd_scene load_steps=3 format=3]
+extends Node
 
-[ext_resource type="Script" path="res://vr_launcher.gd" id="1_l5jd3"]
-[ext_resource type="Texture2D" uid="uid://bemx8dl3tnfsh" path="res://cleveland-state-university.png" id="2_smb8y"]
-
-[node name="VRLauncher" type="Node"]
-script = ExtResource("1_l5jd3")
-
-[node name="Control" type="Control" parent="."]
-layout_mode = 3
-anchors_preset = 15
-anchor_right = 1.0
-anchor_bottom = 1.0
-grow_horizontal = 2
-grow_vertical = 2
-
-[node name="TextureRect" type="TextureRect" parent="Control"]
-layout_mode = 1
-anchors_preset = 15
-anchor_right = 1.0
-anchor_bottom = 1.0
-grow_horizontal = 2
-grow_vertical = 2
-texture = ExtResource("2_smb8y")
-expand_mode = 1
-stretch_mode = 6
-
-[node name="ColorRect" type="ColorRect" parent="Control"]
-layout_mode = 1
-anchors_preset = 15
-anchor_right = 1.0
-anchor_bottom = 1.0
-grow_horizontal = 2
-grow_vertical = 2
-color = Color(0, 0, 0, 0.501961)
-
-[node name="Label" type="Label" parent="Control"]
-layout_mode = 1
-anchors_preset = 8
-anchor_left = 0.5
-anchor_top = 0.5
-anchor_right = 0.5
-anchor_bottom = 0.5
-offset_left = -143.0
-offset_top = -13.0
-offset_right = 143.0
-offset_bottom = 13.0
-grow_horizontal = 2
-grow_vertical = 2
-theme_override_colors/font_shadow_color = Color(0, 0, 0, 1)
-theme_override_constants/shadow_offset_x = 2
-theme_override_constants/shadow_offset_y = 2
-theme_override_font_sizes/font_size = 32
-text = "Loading... Detecting VR hardware..."
+func _ready():
+    # If we're in the editor and running a different scene directly, don't redirect
+    if OS.has_feature("editor") and get_tree().current_scene != self:
+        print("Direct scene testing detected - not redirecting")
+        return
+        
+    # Wait a moment to let XR systems initialize
+    await get_tree().create_timer(0.2).timeout
+    
+    # Check if VR is available
+    var xr_interface = XRServer.find_interface("OpenXR")
+    
+    if xr_interface and xr_interface.is_initialized():
+        print("VR headset detected - launching VR room")
+        get_tree().change_scene_to_file("res://scenes/VR_Room.tscn")
+    else:
+        print("No VR detected - launching desktop menu")
+        get_tree().change_scene_to_file("res://MainMenu.tscn")
